df4806d591d983b3dcbfc50fccaaff8091c3abef
Vladyslav Shesternyov <v.shesternov@knubisoft.com>
2025-09-27 16:05:03 +0300

    add aiohttp server for Render.com to deploy through web service and bot with polling

--- src/bot.py
+++ src/bot.py
@@ -5,6 +5,7 @@ import sys
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiohttp import web
 from dotenv import load_dotenv
 
 from database import engine
@@ -26,22 +27,34 @@ def register_handlers():
     dp.include_router(my_packs.router)
 
 
-async def main():
+async def on_startup(app: web.Application):
     register_handlers()
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    try:
-        await dp.start_polling(bot)
-    except asyncio.CancelledError:
-        logging.info("Bot stopped")
-    finally:
-        await bot.session.close()
+    asyncio.create_task(dp.start_polling(bot))
+
+
+async def on_cleanup(app: web.Application):
+    await bot.session.close()
+
+
+async def healthcheck(request):
+    return web.Response(text="Bot is running!")
+
+
+def main():
+    app = web.Application()
+    app.router.add_get("/", healthcheck)
+    app.on_startup.append(on_startup)
+    app.on_cleanup.append(on_cleanup)
+
+    web.run_app(app, port=int(os.getenv("PORT", 8080)))
 
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        main()
     except KeyboardInterrupt:
         sys.exit(0)
